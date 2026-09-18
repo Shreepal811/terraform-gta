830a40ef40663ac382d14e2a938d3803fef82ca3
Shreepal Singh <rajpurohitshripal89@gmail.com>
2026-09-19 02:34:25 +0530

    new update

--- staging/provider.tf
+++ staging/provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "=5.0.0"
     }
   }
-  backend "azurerm" {
-      resource_group_name  = "testRG"
-      storage_account_name = "mysa12"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
-}
 }
 
 
