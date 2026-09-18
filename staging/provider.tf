ec20b7a14a50dfb732a042958c786b4ec87510d1
Shreepal Singh <rajpurohitshripal89@gmail.com>
2026-09-19 03:14:33 +0530

    remote backend

--- staging/provider.tf
+++ staging/provider.tf
@@ -1,4 +1,10 @@
 terraform {
+    backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "storageaccountremote"
+    container_name        = "tfstate"
+    key                    = "staging.terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
